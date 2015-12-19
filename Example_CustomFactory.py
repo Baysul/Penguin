d1b123defaeb3bb9846d04f518559ca079bc9046
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2015-12-19 11:03:23 -0800

    Moved factory and protocol into example script for simplicity

--- Example_CustomFactory.py
+++ Example_CustomFactory.py
@@ -1,5 +1,35 @@
 from Penguin.ClubPenguin import ClubPenguin
-from Example.MyPenguinFactory import MyPenguinFactory
+from Penguin.ClubPenguin import PenguinFactory
+from Penguin.Penguin import Penguin
+
+class MyPenguin(Penguin):
+
+	def __init__(self, player):
+		super(MyPenguin, self).__init__(player)
+
+		self.addListener("jr", self.handleJoinRoom)
+
+		# Joins room 800 (the dock) at coordinates 223 and 333
+		self.addListener("partycookie", lambda x: self.joinRoom(800, 223, 333))
+
+	def handleJoinRoom(self, data):
+		self.logger.info("Joined room!")
+
+		self.sendPhraseMessage("hi master")
+
+class MyPenguinFactory(PenguinFactory):
+
+	def __init__(self):
+		super(MyPenguinFactory, self).__init__()
+
+		self.logger.debug("MyPenguinFactory constructed")
+
+	def buildProtocol(self, addr):
+		player = self.queue.pop()
+
+		penguin = MyPenguin(player)
+
+		return penguin
 
 cp = ClubPenguin()
 
